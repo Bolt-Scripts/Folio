76731fdaccdff8b6af431f7ea470023016234252
BoltManGuy <boltsoft@gmail.com>
2023-01-24 21:42:22 -0700

    display options and tags

--- js/Index.js
+++ js/Index.js
@@ -3,9 +3,16 @@ $(document).ready(() => ko.applyBindings(new Index()));
 
 function Index() {
 
+    const allIdx = 1;
+
     posts = ko.observableArray();
+    displayType = ko.observable("feed-grid");
+    allTags = ko.observableArray(["Select", "All"]);
+    selectedTags = ko.observableArray([allIdx]);
 
     postsJSON = "";
+
+    tagSelector = $("#tags")[0];
 
     LikePost = function (post) {
         if (post.likeClicked())
@@ -23,10 +30,14 @@ function Index() {
 
     PageLoad = function () {
         let postsRef = database.ref("blogposts");
+
+        //.on will update when new stuff comes in
+        //not super necessary but it is neat
         postsRef.on("value", data => {
             postsJSON = data.val();
             let keys = Object.keys(postsJSON).reverse();
 
+            //return early if no new posts
             if (keys.length <= posts().length)
                 return;
 
@@ -34,12 +45,75 @@ function Index() {
 
             for (let key of keys) {
                 let post = postsJSON[key];
+
+                //handle tags
+                if(post.tags){
+                    let combinedTags = allTags().concat(post.tags);
+                    let uniqueTags = [...new Set(combinedTags)];
+                    allTags(uniqueTags);
+                }else{
+                    post.tags = [];
+                }
+
                 post.id = key;
                 post.likeClicked = ko.observable(false);
                 post.likes = ko.observable(post.likes);
+                post.description = ko.observable(post.description);
+
+                post.filter = function(){
+
+                    if(selectedTags.indexOf(allIdx) >= 0) return true;
+                    if(post.tags.length == 0) return false;
+
+                    for (let i = 0; i < selectedTags().length; i++) {
+                        const tag = allTags()[selectedTags()[i]];
+                        if(post.tags.indexOf(tag) >= 0) return true;
+                    }
+
+                    return false;
+                }
+
                 posts.push(post);
             }
         });
+    }
+
+    SetTagEnable = function (e) {
+        let idx = e.index;
+        
+        let selected = selectedTags.indexOf(idx) >= 0;
+        e.innerText = (selected ? "✓" : "") + allTags()[idx];
+    }
+
+    function SetTagsEnable(select){
+        for (let i = 0; i < select.length; i++) {
+            const opt = select[i];
+            SetTagEnable(opt);
+        }
+    }
+
+    SelectTags = function (contex, e) {
+
+        let idx = e.target.selectedIndex;
+
+        if(idx == allIdx){
+            selectedTags.removeAll();
+        }else{
+            selectedTags.remove(allIdx);
+        }
+
+        if(selectedTags.indexOf(idx) == -1){
+            selectedTags.push(idx);
+        }else{
+            selectedTags.remove(idx);
+
+            if(selectedTags().length == 0){
+                selectedTags.push(allIdx);
+            }
+        }
+        
+        SetTagsEnable(e.target); 
+        e.target.selectedIndex = 0;
     }
 
 
--- js/admin/blog_post.js
+++ js/admin/blog_post.js
@@ -1,45 +1,58 @@
-$(document).ready(() => ko.applyBindings(new BlogPost()));
 
 
 function BlogPost() {
 
-    title = ko.observable();
-    key = ko.observable();
-    msg = ko.observable();
-    imgurl = ko.observable();
+   title = ko.observable();
+   description = ko.observable();
+   key = ko.observable();
+   msg = ko.observable();
+   imgurl = ko.observable("");
+   tags = ko.observable();
 
-    encryptedPage = "U2FsdGVkX191EFhs1mRvNCSdARuy+WF2AFkb5fnxZHyPEXz70VfucfyE8LEG2TyzK2Cr6rT8YCiG+yAwldiDgQ1XpDZMYaxoyPfvuJ8h/KRhsq6pyW48Z7xZ4J0aCm8KG0OtaZyC0KZ8bfZyE5o4eNTeffw5brNtcVkXk4/nuSqLYNEN7ekFGINt67paUzU20wwcXCy8LlIhMt2dKbA+cER8alps8I6EA1Q5ohxVQYHe8Qdl9a7xcsT09c/vWt8Vj5pzA06ua8uxMHPxeOSXqQI/F8NX6oaBZo+wVFtPwduHuoxR+cIXfCXjm+HnIRYqNzmbKTR5xDCx9QkITVvD2G/6rG+vs2x5XlFw3YCltli7IgdtgOyJAbWzFny+TO6G/gcPha7+qcf2Qscs2rM7QpnpQVEpTIhDeqCR+ymMDEF4C4CXvbh+asguZzQ0y4WWOasSM1GUh1nenjVy2l8CK14od7ExHnz1pRA09Iw6/592BgMJcZ/0ugvg9hJ9eOlqz7CJ988a8XaMSx+bYiRc4JDikuzvhkWDMfmyhBcx5dbaGIsn6KPxKjJgQ7ltWPgO+zZm7olJncDLX1Iw5OnqEuHui7rdHzU0EGH5qpye71Vug34XL1hRgVVnPGBswc9qyQHyiVhBh9ZC6TqHa/Ura2hzf57uuq06wwOUrjnbk1Wfz5keOWcRMT9AHS27+S4yEqv8+Wu6k0HCZ0bTk+QTAsUcZrNci6nWZ6900qrdkaA+k87NKzghwwTdJqhGjCfVSFoUWavn48CL5O5an8a4vQ==";
-    encryptedUN = "U2FsdGVkX1+dIw2Su8v8WxWW86FuvuDt9gvfh2veI2A=";
-    let formElm = $("#msform")[0];
+   let encryptedUN = "U2FsdGVkX1+dIw2Su8v8WxWW86FuvuDt9gvfh2veI2A=";
+   let formElm = $("#msform")[0];
 
-    unlock = function(){
-        formElm.innerHTML = CryptoJS.AES.decrypt(encryptedPage, key()).toString(CryptoJS.enc.Utf8);
+    this.unlock = async function () {
         let UN = CryptoJS.AES.decrypt(encryptedUN, key()).toString(CryptoJS.enc.Utf8);
 
-        firebase.auth().signInWithEmailAndPassword(UN, key());
+        firebase.auth().signInWithEmailAndPassword(UN, key())
+            .then(async (creds) => {
 
-        ko.cleanNode(formElm);
-        ko.applyBindings(this, formElm);
+                let response = await fetch('post_form.html');
+                formElm.innerHTML = await response.text();
+                
+                ko.cleanNode(formElm);
+                ko.applyBindings(this, formElm);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
-    post = function () {
+    this.post = function () {
         let ref = database.ref("blogposts");
         let blogpost = {
             title: title(),
+            description: description(),
             msg: msg(),
             imgurl: imgurl(),
-            likes: 0
+            likes: 0,
+            tags: tags().replace(/\s/g,'').split(","),
         };
 
-        ref.push(blogpost, (err)=>{
-            if(!err){
+        console.log(blogpost);        
+
+        ref.push(blogpost, (err) => {
+            if (!err) {
                 formElm.innerHTML += "<p>Succes</p>"
-            }else{
-                formElm.innerHTML += "<p>Failure</p>"                
+            } else {
+                formElm.innerHTML += "<p>Failure</p>"
             }
-            
+
             ko.cleanNode(formElm);
             ko.applyBindings(this, formElm);
         });
     }
 }
+
+$(document).ready(() => ko.applyBindings(new BlogPost()));
